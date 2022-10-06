b1549b557a7499f7ed36e51dab195c1f2ee3764a
RMcCann11 <ronanemccann@hotmail.com>
2022-10-06 10:07:00 +0000

    Have implemented navigation to film-details using filmID

--- mobdev-ionic-intro/src/app/pages/films/films.page.ts
+++ mobdev-ionic-intro/src/app/pages/films/films.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-films',
@@ -9,20 +11,32 @@ import { NavController } from '@ionic/angular';
 })
 export class FilmsPage implements OnInit {
 
-  constructor(private navController: NavController, private router: Router) { }
+  constructor(private navController: NavController, private router: Router, private http: HttpClient) { }
 
+  films: Observable<any>;
+  
   ngOnInit() {
+    this.films = this.http.get('https://swapi.dev/api/films');
+    // this.films.subscribe((data) => {
+    // console.log('my data: ', data);
+    // });
   }
 
-  openDetails() {
-    // Both of these would work!
-    // But the standard Router is recommended.
-    // this.navController.navigateForward(`/tabs/films/42`);
-    this.router.navigateByUrl(`/tabs/films/42`);
-  }
+  // openDetails() {
+  //   // Both of these would work!
+  //   // But the standard Router is recommended.
+  //   // this.navController.navigateForward(`/tabs/films/42`);
+  //   this.router.navigateByUrl(`/tabs/films/42`);
+  // }
  
-  goToPlanets() {
-    this.navController.navigateRoot(`/tabs/planets`)
-  }
+  // goToPlanets() {
+  //   this.navController.navigateRoot(`/tabs/planets`)
+  // }
+
+  openDetails(film) {
+    let split = film.url.split('/');
+    let filmId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/films/${filmId}`);
+    }
 
 }
